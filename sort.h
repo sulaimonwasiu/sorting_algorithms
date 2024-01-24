d5000ff6f51e993294984695965dc30361f4c986
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-24 06:25:49 +0300

    Header modified

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+size_t swap(int *array, size_t size, size_t idx1, size_t idx2);
+void partition(int *array, size_t size, size_t left, size_t right);
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
